2be6eda0e1d14e5215da4862b348d05b94ec3450
Nguyễn Thái Hải <85157423+ThaiHaiDev@users.noreply.github.com>
2023-02-26 11:20:42 +0700

    basic vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MenuTest :dataSend="todoList" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuTest from "./components/MenuTest.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MenuTest,
+  },
+  data() {
+    return {
+      todoList: ["ReactJS", "VueJS", "NodeJS"],
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/MenuTest.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h1>Day la Menu</h1>
+    <!-- Two way binding -->
+    <input type="text" v-model="name"/>
+    <h3>{{ name }}</h3>
+
+    <h1 v-show="isShow">Show name</h1>
+    <button @click="isShow = !isShow">Show</button>
+
+    <h1 :class="{ daChon : selected }">Da chon style</h1>
+    <button @click="selected = !selected">Chon style</button>
+
+    <h2 :class="{ eventClick : testEvent }" v-on:click="() => { testEvent = !testEvent }">Event click</h2>
+
+    <div v-for="(task, index) in dataSend" :key="index">
+      <p v-on:click="() => { handleDemo(task) }">{{ task }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuTest',
+    props: {
+      dataSend: [],
+    },
+    data() {
+      return {
+        name: 'Thai Hai',
+        isShow: true,
+        selected: true,
+        testEvent: false,
+        todoList: [
+          'ReactJS',
+          'VueJS',
+          'NodeJS'
+        ]
+      }
+    },
+    // Chay dau tien khi load, giong useEffect, dung de call Api
+    mounted() {
+
+    },
+    methods: {
+      handleDemo: (task) => {
+        console.log('Function test', task)
+      }
+    },
+    watch: {
+      // Khi data name thay doi thi thuc hien
+      name: function(newValue, oldValue) {
+        console.log('name change')
+        console.log(newValue, oldValue)
+      }
+    }
+};
+</script>
+
+<style>
+.daChon {
+  color: red;
+}
+
+.eventClick {
+  color: blue;
+  cursor: pointer;
+}
+</style>
